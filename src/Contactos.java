eeaf03cfb51b8678a4454609d5541b355fe887e1
JuanC <camiloflstudio@gmail.com>
2022-07-07 08:30:46 -0500

    Se agrego funcionalidad agregar, salir, falta recorrer el array para la opcion listar

--- src/Contactos.java
+++ src/Contactos.java
@@ -1,5 +1,50 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Contactos{
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        
+        String nombre;
+        String celular;
+
+        boolean estado = true;
+        Scanner leerOpcion = new Scanner(System.in);
+        Scanner leerDato = new Scanner(System.in);
+        ArrayList<ArrayList> listaContactos = new ArrayList<>();
+        
+        do{
+            System.out.println("LISTA CONTACTOS\n 1.Ver contactos\n 2.Agregar\n 3.Eliminar\n 4.Modificar\n 5.Salir");
+            System.out.println("Digite la Opcion: ");
+            int opcion = leerOpcion.nextInt();
+            switch(opcion){
+
+                case 1:
+                    if(listaContactos.isEmpty()){
+                        System.out.println("No existen contactos");
+                    } else {
+                        System.out.println("Contactos existentes: ");
+                    }
+                    break;
+                
+                case 2: 
+                    ArrayList<String> contactos = new ArrayList<>();
+                    System.out.println("Nombre: ");
+                    nombre = leerDato.nextLine();
+                    System.out.println("Celular: ");
+                    celular = leerDato.nextLine();
+                    contactos.add(nombre);
+                    contactos.add(celular);
+                    listaContactos.add(contactos);
+                    System.out.println(listaContactos);
+                    System.out.println("Contacto guardado");
+                    break;
+
+                case 5:
+                    estado = false;
+                    System.out.println("Hasta pronto!!");
+                break;
+            }
+        }while(estado);
+
     }
-}
+} 
